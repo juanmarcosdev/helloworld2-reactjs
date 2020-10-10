3649de8df912784af290bae2874054115a737884
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-10-10 13:50:18 -0500

    Adding custom Hook

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
@@ -6,35 +6,55 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const API = 'http://localhost:3000/initalState';
 
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+const App = () => {
+  const initialState = useInitialState(API);
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      {
+        initialState.mylist.length > 0 && (
+          <Categories title='Mi lista'>
+            <Carousel>
+              {
+                initialState.mylist.map((item) => <CarouselItem key={item.id} {...item} />)
+              }
+            </Carousel>
+          </Categories>
+        )
+      }
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      {
+        initialState.trends.length > 0 && (
+          <Categories title='Tendencias'>
+            <Carousel>
+              {
+                initialState.trends.map((item) => <CarouselItem key={item.id} {...item} />)
+              }
+            </Carousel>
+          </Categories>
+        )
+      }
 
-    <Categories title='Originales de JSVideo'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+      {
+        initialState.originals.length > 0 && (
+          <Categories title='Originales de JSVideo'>
+            <Carousel>
+              {
+                initialState.originals.map((item) => <CarouselItem key={item.id} {...item} />)
+              }
+            </Carousel>
+          </Categories>
+        )
+      }
+
+      <Footer />
+    </div>
+  );
+};
 
 export default App;

82920a92c01e9b2a058fab5ba553f0f364e317b1
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-10-11 11:16:27 -0500

    Adding reducer to delete from my list

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -8,33 +8,38 @@ import plusIcon from '../assets/static/plus-icon.png';
 import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
-  const { id, cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration, isList } = props;
   const handleSetFavorite = () => {
     props.setFavorite({
       id, cover, title, year, contentRating, duration,
     });
   };
   const handleDeleteFavorite = (itemId) => {
-    props.deleteFavorite(itemId)
-  }
+    props.deleteFavorite(itemId);
+  };
   return (
     <div className='carousel-item'>
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
           <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
-          <img
-            className='carousel-item__details--img'
-            src={plusIcon}
-            alt='Plus Icon'
-            onClick={handleSetFavorite}
-          />
-          <img
-            className='carousel-item__details--img'
-            src={removeIcon}
-            alt='Remove Icon'
-            onClick={() => handleDeleteFavorite(id)}
-          />
+          {
+            isList ? (
+              <img
+                className='carousel-item__details--img'
+                src={removeIcon}
+                alt='Remove Icon'
+                onClick={() => handleDeleteFavorite(id)}
+              />
+            ) : (
+              <img
+                className='carousel-item__details--img'
+                src={plusIcon}
+                alt='Plus Icon'
+                onClick={handleSetFavorite}
+              />
+            )
+          }
         </div>
         <p className='carousel-item__details--title'>{title}</p>
         <p className='carousel-item__details--subtitle'>
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -16,7 +16,11 @@ const Home = ({ mylist, trends, originals }) => {
           <Categories title='Mi lista'>
             <Carousel>
               {
-                mylist.map((item) => <CarouselItem key={item.id} {...item} />)
+                mylist.map((item) => <CarouselItem 
+                key={item.id} 
+                {...item}
+                isList
+                 />)
               }
             </Carousel>
           </Categories>

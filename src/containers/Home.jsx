320dd7dd777d8830f093de10f6962caecdfc0167
juanmarcosdev <juanmarcoscaicedo@gmail.com>
2020-10-10 22:22:00 -0500

    Adding layout (header and footer rendering) to all pages

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../components/Header';
 import '../assets/styles/App.scss';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initalState';
@@ -13,8 +11,7 @@ const API = 'http://localhost:3000/initalState';
 const Home = () => {
   const initialState = useInitialState(API);
   return (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
       {
         initialState.mylist.length > 0 && (
@@ -51,9 +48,7 @@ const Home = () => {
           </Categories>
         )
       }
-
-      <Footer />
-    </div>
+    </>
   );
 };
 
